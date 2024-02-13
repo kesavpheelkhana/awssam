e23a9917d1b0c8fec9a136d4aa108528b3890e33
ckesav <keshav.pheelkhana@gmail.com>
2024-02-13 22:24:57 +0530

    add new rolldice api

---
+++ sam-first-project/roll-dice/app.mjs
@@ -0,0 +1,21 @@
+export const lambdaHandler = async(event) => {
+    console.log("Roll Dice Run!");
+
+    const result = rollDice(6);
+
+    try{
+        return {
+            statusCode :200,
+            body: JSON.stringify({message : result})
+        }
+    }catch(err){
+        console.log(err);
+        return err;
+    }
+};
+
+function rollDice(sides){
+    const randomNumber = Math.floor(Math.random() * sides) + 1;
+    return randomNumber;
+}
+
